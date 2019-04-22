1861bc03164126df004aaaf82ed870602d24f701
Melissa Real <melissa.real@hotmail.com>
2019-04-22 12:22:24 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,12 +2,12 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   document.body.addEventListener('keydown', function(e){
-  const key = parseInt(e.detail || e.which);
 
+  const key = parseInt(e.detail || e.which);
   if (key === code[pressed]) {
     pressed++;
 
-    if (pressed === code.length) {
+    if (pressed.length === code.length) {
       alert("Holeee!");
       index = 0;
     }
